cc572883b477ad338856f94c776371906827c098
Nivya212 <nivyanelson043@gmail.com>
2024-05-01 23:48:04 +0530

    db

--- Wish/models.py
+++ Wish/models.py
@@ -15,27 +15,16 @@ class List(models.Model):
     wishlist_name =  models.CharField(max_length=255)
     description =models.TextField(null=True,blank=True)
 
-    def __str__(self):
+    def _str_(self):
         return str(self.wishlist_id)
     
+    
 class ListItem(models.Model):
-    wishlist = models.ForeignKey(List, on_delete=models.CASCADE, related_name='items')
-    item_name = models.CharField(max_length=255,)
-    item_id = models.CharField(max_length=255,default=None)
+    wishlist= models.ForeignKey(List, on_delete=models.CASCADE, related_name='items')
+    item_name = models.CharField(max_length=255)
+    status = models.CharField(max_length=255,null=True)
     
     # Add more fields if needed 
 
-    def __str__(self):
-        return self.item_name
-
-class ItemDetails(models.Model):
-    item_name = models.CharField(max_length=255,null=True)
-    item_id = models.ForeignKey(ListItem, on_delete=models.CASCADE, blank=True,default=None)
-    description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True) 
-    status = models.CharField(max_length=255, null=True, blank=True)
-    link = models.URLField(null=True, blank=True)
-    image = models.ImageField(upload_to='wishlists/', null=True, blank=True)  
-
-    def __str__(self):
+    def _str_(self):
         return self.item_name
